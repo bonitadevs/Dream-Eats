c6293dfbb3a39ecf8a3b31a378a93c62e8e5029a
Dani <ddw29@me.com>
2021-10-19 18:36:02 -0400

    added tab menu with icons

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './HomeComponent'
-import Recipe from './RecipesComponent'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Home from './HomeComponent';
+import Recipe from './RecipesComponent';
 import Favorites from './FavoritesComponent';
-import ShoppingList from './ShoppingList';
+import ShoppingList from './ShoppingListComponent';
 
 
 
@@ -15,7 +16,30 @@ const Tab = createBottomTabNavigator();
 export default function Main() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } if (route.name === 'Recipes') {
+              iconName = focused ? 'fast-food' : 'fast-food-outline';
+            } if (route.name === 'Favorites') {
+              iconName = focused ? 'heart' : 'heart-outline';
+            } if (route.name === 'Shopping List') {
+              iconName = focused ? 'cart' : 'cart-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'purple',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Recipes" component={Recipe} />
         <Tab.Screen name="Favorites" component={Favorites} />
--- components/RecipesComponent.js
+++ components/RecipesComponent.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 function Recipe () {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
+        <Text>Recipes!</Text>
       </View>
     );
   }
